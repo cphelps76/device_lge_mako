792020ea1f49f19b549a4e7ff061976f52970038
cphelps76 <cphelps76@gmail.com>
2013-06-05 23:07:03 -0500

    defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/lge/mako
-TARGET_KERNEL_CONFIG := mako_defconfig
+TARGET_KERNEL_CONFIG := DEMENTED_mako_defconfig
 
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_FLUENCE_INCALL := true
